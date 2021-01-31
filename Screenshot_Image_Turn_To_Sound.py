0435e04928a9bde52b82546f54c0bf323dc54be4
黃敏雄 <64833384+MinXiong-Huang@users.noreply.github.com>
2021-01-31 12:16:02 +0800

    Update Screenshot_Image_Turn_To_Sound.py

--- Screenshot_Image_Turn_To_Sound.py
+++ Screenshot_Image_Turn_To_Sound.py
@@ -12,7 +12,7 @@ def Upper_left_corner():
     while True:
         if keyboard.is_pressed('z'):
             global pos1
-            pos1=pyautogui.position()
+            pos1 = pyautogui.position()
             print(pos1)
             break
 
@@ -22,14 +22,14 @@ def Lower_right_corner():
     while True:
         if keyboard.is_pressed('x'):
             global pos2
-            pos2=pyautogui.position()
+            pos2 = pyautogui.position()
             print(pos2)
             break
 
 #偵測圖像文字且轉為音檔
 def Detect_image_text_and_turn_to_sound():
-    Img_path=r'.\img.png'
-    Detect_Text=pytesseract.image_to_string(Img_path) #偵測文字
+    Img_path = r'.\img.png'
+    Detect_Text = pytesseract.image_to_string(Img_path) #偵測文字
     print('內容:\n'+Detect_Text)
 
     with open(r".\Detect_Text.txt","w") as f:
@@ -37,7 +37,7 @@ def Detect_image_text_and_turn_to_sound():
         #print('已寫入文字檔!')
         pyautogui.alert('已寫入文字檔!')
 
-    Convert_Sound=gTTS(Detect_Text,lang='en') #轉為音檔
+    Convert_Sound = gTTS(Detect_Text,lang='en') #轉為音檔
     Convert_Sound.save(r'.\Sound.mp3') #儲存音檔
     pyautogui.alert('已存成音檔!')
     #print('已存成音檔!')
@@ -45,11 +45,11 @@ def Detect_image_text_and_turn_to_sound():
 if __name__ == "__main__":
     Upper_left_corner()
     Lower_right_corner()
-    x=pos1.x
-    y=pos1.y
-    w=pos2.x-pos1.x
-    h=pos2.y-pos1.y
-    img=pyautogui.screenshot(region=(x,y,w,h))
+    x = pos1.x
+    y = pos1.y
+    w = pos2.x-pos1.x
+    h = pos2.y-pos1.y
+    img = pyautogui.screenshot(region=(x,y,w,h))
     img.save(r'.\img.png')
     pyautogui.alert('截圖已成功!')
     Detect_image_text_and_turn_to_sound()
